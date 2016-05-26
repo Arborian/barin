3c3a4e6c57af8315989bc0d0bc43de3305703947
Rick Copeland <rick@arborian.com>
2016-05-26 18:40:42 -0400

    Add shorthand schema compiler
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- barin/schema/__init__.py
+++ barin/schema/__init__.py
@@ -1,4 +1,4 @@
 from .base import Invalid, Missing, Schema, Anything
-
 from .scalar import Scalar, ObjectId, Number, Integer, Float, Unicode
 from .compound import Document, Array
+from .compiler import compile_schema
