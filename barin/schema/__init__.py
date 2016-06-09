c0025a82d928f5b44d5964fed9bddb67bffbad17
Rick Copeland <rick@arborian.com>
2016-06-09 11:54:52 -0400

    Add missing import
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- barin/schema/__init__.py
+++ barin/schema/__init__.py
@@ -1,4 +1,4 @@
 from .base import Invalid, Missing, Validator, Anything
-from .scalar import Scalar, ObjectId, Number, Integer, Float, Unicode
+from .scalar import Scalar, ObjectId, Number, Integer, Float, Unicode, DateTime
 from .compound import Document, Array
 from .compiler import compile_schema
