155674adbd611cbb9aab39e03e69022fa1b983db
Rick Copeland <rick@arborian.com>
2016-06-18 18:13:45 -0400

    and_ and or_ improvements in mql
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- barin/mql.py
+++ barin/mql.py
@@ -23,42 +23,22 @@ def comment(value):
 
 def _logical_nary_op(op):
     def inner(*parts):
-        cur = None
         for part in parts:
             if not isinstance(part, dict):
                 raise errors.QueryError(
                     'Illegal {} clause: {}'.format(op, part))
-            if cur is None:
-                cur = dict(part)
-                continue
-            cur = {op: [cur, part]}
-        return cur
-    inner.__name__ = op
-    return inner
-
-
-def _logical_nary_op(op):
-    def inner(*parts):
-        cur = None
-        for part in parts:
-            if not isinstance(part, dict):
-                raise errors.QueryError(
-                    'Illegal {} clause: {}'.format(op, part))
-        return {op: parts}
+        return {op: list(parts)}
     inner.__name__ = op
     return inner
 
 
 def and_(*parts):
     """Try to build a compound document without using $and."""
-    result = None
+    result = {}
     for part in parts:
         if not isinstance(part, dict):
             raise errors.QueryError(
                 'Illegal $and clause: {}'.format(part))
-        if result is None:
-            result = part
-            continue
         for k, v in part.items():
             r_v = result.setdefault(k, {})
             if set(v.keys()).intersection(r_v):
