b0e6dfb1101e28533d439b384e8e7681592cccec
Rick Copeland <rick@arborian.com>
2017-09-02 13:37:09 -0400

    Add missing files
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ barin/tests/test_fields.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from barin import field
+from barin import metadata
+
+
+class TestFields(TestCase):
+
+    def setUp(self):
+        self.fields = field.FieldCollection([
+            field.Field('x', int)])
+        self.metadata = metadata.Metadata()
+
+    def test_make_schema(self):
+        self.fields.make_schema(self.metadata)
