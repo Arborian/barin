d0ac48c07643ce24230f1fc985ed678fda9120aa
Rick Copeland <rick@arborian.com>
2021-02-24 09:47:57 -0500

    Code style updates, add UUID type

--- barin/schema/__init__.py
+++ barin/schema/__init__.py
@@ -1,7 +1,35 @@
-from .base import Invalid, Missing, Validator, Anything, Strip      # noqa
-from .scalar import (                                               # noqa
-    Scalar, ObjectId, DBRef, Number, Integer, Float,
-    Unicode, DateTime, Binary,
+__all__ = (
+    "Invalid",
+    "Missing",
+    "Validator",
+    "Anything",
+    "Strip",
+    "Scalar",
+    "ObjectId",
+    "DBRef",
+    "Number",
+    "Integer",
+    "Float",
+    "Unicode",
+    "DateTime",
+    "Binary",
+    "UUID",
+    "Document",
+    "Array",
+    "compile_schema",
 )
-from .compound import Document, Array                               # noqa
-from .compiler import compile_schema                                # noqa
+from .base import Invalid, Missing, Validator, Anything, Strip
+from .scalar import (
+    Scalar,
+    ObjectId,
+    DBRef,
+    Number,
+    Integer,
+    Float,
+    Unicode,
+    DateTime,
+    Binary,
+    UUID,
+)
+from .compound import Document, Array
+from .compiler import compile_schema
--- barin/schema/scalar.py
+++ barin/schema/scalar.py
@@ -13,54 +13,46 @@ class Scalar(Validator):
 
 
 class ObjectId(Scalar):
-    _msgs = dict(
-        Scalar._msgs,
-        not_oid='Value must be an ObjectId')
+    _msgs = dict(Scalar._msgs, not_oid="Value must be an ObjectId")
 
     def _validate(self, value, state=None):
         if not isinstance(value, bson.ObjectId):
-            raise Invalid(self._msgs['not_oid'], value)
+            raise Invalid(self._msgs["not_oid"], value)
         return value
 
 
 class DBRef(Scalar):
-    _msgs = dict(
-        Scalar._msgs,
-        not_dbref='Value must be an DBRef')
+    _msgs = dict(Scalar._msgs, not_dbref="Value must be an DBRef")
 
     def _validate(self, value, state=None):
         if not isinstance(value, bson.dbref.DBRef):
-            raise Invalid(self._msgs['not_dbref'], value)
+            raise Invalid(self._msgs["not_dbref"], value)
         return value
 
 
 class Number(Scalar):
-    _msgs = dict(
-        Scalar._msgs,
-        not_num='Value must be an number')
+    _msgs = dict(Scalar._msgs, not_num="Value must be an number")
 
     def _validate(self, value, state=None):
         if not isinstance(value, (six.integer_types, float)):
-            raise Invalid(self._msgs['not_num'], value)
+            raise Invalid(self._msgs["not_num"], value)
         return value
 
 
 class Integer(Number):
-    _msgs = dict(
-        Number._msgs,
-        not_int='Value must be an integer')
+    _msgs = dict(Number._msgs, not_int="Value must be an integer")
 
     def _validate(self, value, state=None):
         value = super(Integer, self)._validate(value, state)
         if not isinstance(value, six.integer_types):
-            raise Invalid(self._msgs['not_int'], value)
+            raise Invalid(self._msgs["not_int"], value)
         return value
 
 
 class Float(Number):
     _msgs = dict(
-        Number._msgs,
-        not_float='Value must be a floating-point number')
+        Number._msgs, not_float="Value must be a floating-point number"
+    )
 
     def _validate(self, value, state=None):
         value = super(Float, self)._validate(value, state)
@@ -68,33 +60,29 @@ class Float(Number):
 
 
 class Unicode(Scalar):
-    _msgs = dict(
-        Scalar._msgs,
-        not_unicode='Value must be a Unicode string')
+    _msgs = dict(Scalar._msgs, not_unicode="Value must be a Unicode string")
 
     def _validate(self, value, state=None):
         if not isinstance(value, six.string_types):
-            raise Invalid(self._msgs['not_unicode'], value)
+            raise Invalid(self._msgs["not_unicode"], value)
         return value
 
 
 class Binary(Scalar):
     _msgs = dict(
-        Scalar._msgs,
-        not_binary='Value must be a bson.Binary or bytes object')
+        Scalar._msgs, not_binary="Value must be a bson.Binary or bytes object"
+    )
 
     def _validate(self, value, state=None):
         try:
             return bson.Binary(value)
-        except TypeError as err:
-            raise Invalid(self._msgs['not_binary'], value)
+        except TypeError:
+            raise Invalid(self._msgs["not_binary"], value)
         return value
 
 
 class DateTime(Scalar):
-    _msgs = dict(
-        Scalar._msgs,
-        not_dt='Value must be a datetime object')
+    _msgs = dict(Scalar._msgs, not_dt="Value must be a datetime object")
 
     def _validate(self, value, state=None):
         """Convert to a default datetime on egress from the DB."""
@@ -102,8 +90,18 @@ class DateTime(Scalar):
         if not isinstance(res, datetime) and isinstance(res, date):
             res = datetime.combine(res, time.min)
         if not isinstance(res, datetime):
-            raise Invalid(self._msgs['not_dt'], value)
+            raise Invalid(self._msgs["not_dt"], value)
         if res.tzinfo:
             res = res.astimezone(pytz.utc)
             res = res.replace(tzinfo=None)
+        return res
+
+
+class UUID(Scalar):
+    _msgs = dict(Scalar._msgs, not_uuid="Value is not a valid UUID")
+
+    def _validate(self, value, state=None):
+        res = super(DateTime, self)._validate(value, state)
+        if not isinstance(res, UUID):
+            raise Invalid(self._msgs["not_uuid"], value)
         return res
