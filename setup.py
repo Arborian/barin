a2c0f3a56bb9653ebbd1bba0d4c64eaf2152d50f
Rick Copeland <rick@arborian.com>
2022-10-05 19:36:13 +0000

    Setup changes, bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='barin',
-#    version='0.0.5',
-    version_format='{tag}.dev{commitcount}',
-    setup_requires=['setuptools-git-version'],
-    description='Barin, yet another MongoDB schema validation layer',
-    long_description='Some restructured text maybe',
+    name="barin",
+    version="0.0.6",
+    setup_requires=[],
+    description="Barin, yet another MongoDB schema validation layer",
+    long_description="Some restructured text maybe",
     classifiers=[
         "Programming Language :: Python",
     ],
-    author='Rick Copeland',
-    author_email='rick@arborian.com',
-    url='',
-    keywords='',
+    author="Rick Copeland",
+    author_email="rick@arborian.com",
+    url="",
+    keywords="",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -21,4 +20,5 @@ setup(
     tests_require=[],
     entry_points="""
     [console_scripts]
-    """)
+    """,
+)
